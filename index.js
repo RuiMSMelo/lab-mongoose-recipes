a333f9b62bcf22faba8cc697f2b58060bd222f82
RuiMSMelo <ruimsmelo@gmail.com>
2023-04-21 15:27:33 +0100

    finished lab, after hours i know

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const mongoose = require('mongoose');
+mongoose.set('strictQuery', false);
 
 // Import of the model Recipe from './models/Recipe.model.js'
 const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
+// mongodb://127.0.0.1:27017/recipe-app
+// mongodb://localhost:27017
+const MONGODB_URI = 'mongodb://127.0.0.1:27017/recipe-app';
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
-
+const pizzaPepperoni = {
+  title: 'Pepperoni Pizza',
+  level: 'Easy Peasy',
+  cuisine: 'Italian',
+  ingredients: ['dough', 'tomato sauce', 'cheese', 'pepperoni']
+}
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI)
@@ -16,7 +24,26 @@ mongoose
     return Recipe.deleteMany()
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    return Recipe.create(pizzaPepperoni)
+  })
+  .then((responseFromDatabase) => {
+    console.log(responseFromDatabase)
+    return Recipe.insertMany(data)
+  })
+  .then((responseFromDatabase) => {
+    console.log(responseFromDatabase)
+    return Recipe.findOneAndUpdate({title: "Rigatoni alla Genovese"}, {duration: 100}, {new: true})
+  })
+  .then((responseFromDatabase) => {
+    console.log("updated the db:", responseFromDatabase)
+    return Recipe.deleteOne({title: "Carrot Cake"})
+  })
+  .then((responseFromDatabase) => {
+    console.log("deleted: ", responseFromDatabase)
+    return mongoose.connection.close()
+  })
+  .then (() => {
+    console.log("closed connection")
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
